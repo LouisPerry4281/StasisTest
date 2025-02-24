0b03c2de6c4f47cf58217f2b214b75619069c710
LouisPerry4281 <louisperry4281@gmail.com>
2025-02-24 10:13:41 +0000

    Stasis Object Launched on Hit

--- StasisProject/Assets/StasisCollision.cs
+++ StasisProject/Assets/StasisCollision.cs
@@ -2,8 +2,24 @@ using UnityEngine;
 
 public class StasisCollision : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    [SerializeField] private float launchForce;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     public void HitStasisObject(Vector3 positionHit)
     {
-        print("Collided");
+        Vector3 directionToLaunch = transform.position - positionHit;
+
+        LaunchObject(directionToLaunch, positionHit);
+    }
+
+    private void LaunchObject(Vector3 directionToLaunch, Vector3 positionToHit)
+    {
+        rb.AddForceAtPosition(directionToLaunch * launchForce, positionToHit);
     }
 }
