5aae76ea9f33bf88798d91fa87c858366965a74d
LouisPerry4281 <louisperry4281@gmail.com>
2025-03-01 20:26:34 +0000

    Direction Indicator Renders On Top

--- StasisProject/Assets/DirectionIndicator.cs
+++ StasisProject/Assets/DirectionIndicator.cs
@@ -4,7 +4,7 @@ public class DirectionIndicator : MonoBehaviour
 {
     public void SetDirection(Vector3 position, Vector3 directionToFace)
     {
-        transform.position = new Vector3(position.x, position.y + 2, position.z);
+        transform.position = new Vector3(position.x, position.y + 0.5f, position.z);
         Vector3 positionToFace = transform.position + directionToFace;
         transform.LookAt(positionToFace);
     }
